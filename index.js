a08a52ed36a0a3067170ad270367462775620c44
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2023-09-23 02:34:08 +0530

    cors trial1

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(express.json());
 
 // Middleware for cors policy
-const corsOptions = {
-  origin: "https://booklib-vidhyasagar.netlify.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "https://booklib-vidhyasagar.netlify.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.get("/", (request, response) => {
